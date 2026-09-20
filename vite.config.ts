e1c26e30f785a29367e309f866c9ddfd7f19dded
eh3ankazemi <eh3ankazemii@gmail.com>
2026-09-20 03:42:19 +0330

    fix Vercel server deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
@@ -15,5 +16,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    nitro(),
   ],
 });
